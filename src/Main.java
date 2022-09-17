ddb15b5f130e672c8e02a6266326a5c3ffc654f4
LocNguyen <nguye2lc@mail.uc.edu>
2022-09-17 15:02:44 -0400

    Lab_04_01_SalesTax

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        double taxPrice=0;
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter the price of an item");
+
+        double price = myObj.nextDouble();
+        taxPrice = price * 5/100;
+        System.out.println("Sales tax is: " + taxPrice);
+
     }
 }
